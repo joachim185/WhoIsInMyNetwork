492a86ba40d9f049a8c506617abbfa4d511e50ae
joachim185 <59223959+joachim185@users.noreply.github.com>
2021-03-28 14:35:34 +0200

    Add files via upload

--- WhoIsInMyNetwork.py
+++ WhoIsInMyNetwork.py
@@ -1,85 +1,85 @@
-#To do: Global keyword checken
-#Bughunting
-
-
-
-
+# To do: Check nslookup error
+# Bughunting
 
 import subprocess as sp
 import os
 import time
 
-gloabal output
-output=[]
+
+def glob():
+    global output
+    output = []
+
 
 def ping():
+    try:
+        print("Generating (temporary) hostfile")
+        os.system("fping -g 192.168.8.1 192.168.8.200 | grep alive > host")
+    except:
+        print("An error has occurred!!")
 
-	try:
-		print("Generating (temporary) hostfile")
-		os.system("fping -g 192.168.8.1 192.168.8.200 | grep alive > host")
-	except:
-		print("An error has occurred!!")
+    os.system("clear")
 
-	os.system("clear")
 
 def getName():
+    f = open("host", "r")
 
-	f=open("list.txt","r")
+    for i in f.readlines():
 
-	for i in f.readlines():
-		
-		txt=i.replace("\n","")
+        txt = i.replace(" is alive", "")
+        txt = txt.replace("\n", "")
 
-		try:
-			host=sp.check_output(["nslookup", txt]).decode("utf-8")
-			posA=host.find("=")
-			posB=host.find("Auth")
-			name=txt +" ------> "+host[posA+2:posB-3]
-			print(name)
-			output.append(name)
+        try:
+            host = sp.check_output(["nslookup", txt]).decode("utf-8")
+            posA = host.find("=")
+            posB = host.find("Auth")
+            name = txt + " ------> " + host[posA + 2:posB - 3]
+            print(name)
+            output.append(name)
 
-		except:
-			print("An error has occurred!!")
-
-def fileHandling(k,w):
-	
-	if k.upper()=="Y":
-		print("Keeping the hostfile")
-
-	elif k.upper()=="N":
-		print("Deleting the hostfile")
-		os.system("rm host")
-	
-	else:
-		print("Can't understand what you want me to do with the hostfile. I'm keeping it.")
+        except:
+            print("An error has occurred! IP= "+txt)
 
 
-	if w.upper()=="Y":
-		print("Writing the output to 'OUTPUT.txt'. ")
-		os.system("echo "+output+ " >>OUTPUT.txt")
+def fileHandling(k, w):
+    if k.upper() == "Y":
+        print("Keeping the hostfile")
 
-	elif k.upper()=="N":
-		pass
-	
-	else:
-		print("Writing the output to 'OUTPUT.txt'. ")
-		os.system("echo "+output+ " >>OUTPUT.txt")
-	
-	
+    elif k.upper() == "N":
+        print("Deleting the hostfile")
+        os.system("rm host")
+
+    else:
+        print("Can't understand what you want me to do with the hostfile. I'm keeping it.")
+
+    if w.upper() == "Y":
+        print("Writing the output to 'OUTPUT.txt'. ")
+
+        outPut=str(output).replace("[","")
+        ooutPut = str(outPut).replace("]", "")
+        os.system("echo "+ str(ooutPut) + " > OUTPUT.txt")
+
+    elif k.upper() == "N":
+        pass
+
+    else:
+        print("Writing the output to 'OUTPUT.txt'. ")
+        outPut = str(output).replace("[", "")
+        ooutPut = str(outPut).replace("]", "")
+        os.system("echo " + str(ooutPut) + " > OUTPUT.txt")
 
 
 
 
+glob()
 ping()
 getName()
 
-keep=input("Do you want to keep the hostfile? (Y/N)")
-write=input("Do you want to write the output into a file? (Y/N)")
+keep = input("Do you want to keep the hostfile? (Y/N)")
+write = input("Do you want to write the output into a file? (Y/N)")
 
-fileHandling(keep,write)
-
+fileHandling(keep, write)
 
 print("Goodby!")
 
 time.sleep(3)
-
